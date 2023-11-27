e4e333295d924e941495a8639009e706e62b060f
Andy ramaroson <lemurianagency@gmail.com>
2023-11-27 17:35:02 +0100

    feat(navigation-components-home) update page home with navigation desktop and mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import Navigation from './../components/Navigation'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#f8f3f3] overflow-x-hidden w-full`}>
+        <Navigation />
         {children}
       </body>
     </html>
